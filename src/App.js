06f1f22166dc6e0be59747edd3478083eef335af
Sakshi Gairola <1906499@kiit.ac.in>
2021-01-02 16:28:12 +0530

    broken our homepage into smaller components, finished styling the menu-item components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component.jsx';
+import HomePage from './pages/homepage/homepage.component.jsx';
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <HomePage />
     </div>
   );
 }
